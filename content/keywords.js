cd4db31f05198a7de02f5f17d3a571cae053442a
caryaharper <caryaharper@gmail.com>
2022-03-01 19:11:41 -0600

    created examples of how to create variables in javascript

---
+++ content/keywords.js
@@ -0,0 +1,7 @@
+//these are the 4 ways to declare a variable in JavaScript
+//line 4 is not using a decleration keyword and therefore is not possible in strict mode
+
+foo = 'foo';
+var bar = 'bar';
+let baz = 'baz';
+const fooBar = 'fooBar';
